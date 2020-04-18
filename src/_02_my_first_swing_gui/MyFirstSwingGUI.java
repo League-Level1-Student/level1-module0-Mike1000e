1871013d25e98ef6818b06e9f18a3661fa14346d
Mike1000e <denis.luckash@gmail.com>
2020-04-18 11:26:38 -0700

    frames and stuff

--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,32 +16,33 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Create and initialize an object of the JFrame class
-
+		JFrame jfRAME = new JFrame();
 		// 2. Set your JFrame object to be visible
-
+		jfRAME.setVisible(true);
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		jfRAME.setDefaultCloseOperation(jfRAME.EXIT_ON_CLOSE);
 		// 5. Create and initialize an object of the JPanel class
-
+		JPanel jpANEL = new JPanel();
 		// 6. Create and initialize an object of the JLabel class
-
+		JLabel jlABEL = new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+		jlABEL.setText("Lovely Greeting");
 		// 8. Add the JPanel object to the JFrame
-
+		jfRAME.add(jpANEL);
 		// 9. Add the JLabel object to the JPanel
-
+		jpANEL.add(jlABEL);
 		// 10. Pack your JFrame.
-
+		jfRAME.pack();
 		// 11. Run your program again. Do you see your message.
-
+		
 		// 12. Use the loadImage method to set the icon of the JLabel object.
-
+		
+		jlABEL.setIcon(loadImage());
 		// 13. Re-pack the JFrame object.
-
+		jfRAME.pack();
 		// 14. Run the program one more time. Do you see the image?
 
 	}
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -22,39 +22,54 @@ public class PhotoQuiz {
 		quizWindow.setVisible(true);
 		// This will make sure the program exits when you close the window
 		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		int score = 0;
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+		String image = "https://images-na.ssl-images-amazon.com/images/I/61W6nIN3UjL._AC_SX425_.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+        Component holdIm;
 		// 3. use the "createImage()" method below to initialize your Component
-
+        holdIm = createImage(image);
 		// 4. add the image to the quiz window
-
+        quizWindow.add(holdIm);
 		// 5. call the pack() method on the quiz window
-
+        quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+        String que  = JOptionPane.showInputDialog("What was the first working yoyo made of?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+        if(que.equalsIgnoreCase("Wood")) {
+        	System.out.println( "Correct");
+        	score++;
+        }else {
+        	System.out.println( "Incorrect");
+        }
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+        quizWindow.remove(holdIm);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+        String secImage = "https://i.redd.it/yan4siuhhow21.png";
+       
+        Component holdTwo;
+        
+        holdTwo = createImage(secImage);
 		// 11. add the second image to the quiz window
-
+        quizWindow.add(holdTwo);
 		// 12. pack the quiz window
-
+        quizWindow.pack();
 		// 13. ask another question
-
+        String queT  = JOptionPane.showInputDialog("Who inspired me to do yoyo's");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+        if(queT.equalsIgnoreCase("Pewdiepie")) {
+        	System.out.println( "Correct");
+        	score++;
+        }else {
+        	System.out.println( "Incorrect");
+        }
+        JOptionPane.showMessageDialog(null, "Your scor is " + score + "/2");
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
